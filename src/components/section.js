6681b907e4a128be57d72270f5ebb86820d2f3b1
jtsdev <jtsjordan1@gmail.com>
2020-03-05 14:59:42 -0800

    fixed links and deployed

--- src/components/section.js
+++ src/components/section.js
@@ -73,7 +73,7 @@ class Home extends React.Component {
                     <h3 className="h3">Destiny 2 Companion App</h3>
                     <div className="button_container">
                       <a
-                        href="https://jtsdev.github.io/transmat-landing"
+                        href="https://jtsdev.github.io/transmat-landing/"
                         className="link_style_1 launch"
                       >
                         <span>Launch App</span>
@@ -343,7 +343,7 @@ class Demo extends React.Component {
                     <li>Item transfers disabled.</li>
                   </ul>
                   <a
-                    href="https://jtsdev.github.io/transmat-react"
+                    href="https://jtsdev.github.io/transmat-react/"
                     className="link_style_1 launch demo"
                   >
                     <span>Launch Demo</span>
